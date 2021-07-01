2a0dc1ac6ef6bcb922fb62285809ec4805ef97f8
Salohiddin <berdiyarovsalohiddin@gmail.com>
2021-07-01 16:00:27 +0500

    Applications - CRUD

---
+++ app/Http/Controllers/Admin/ApplicationsController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Services\ApplicationService;
+use Illuminate\Http\Request;
+
+class ApplicationsController extends Controller
+{
+    protected $applicationsService;
+
+    public function __construct()
+    {
+        $this->applicationsService = app(ApplicationService::class);
+    }
+
+    public function index()
+    {
+        $applications = $this->applicationsService->getPaginatedApplication();
+        return view('admin.applications.index', compact('applications'));
+    }
+
+    public function done(int $id)
+    {
+        return $this->applicationsService->done($id);
+    }
+
+    public function destroy($id)
+    {
+        return $this->applicationsService->deleteData($id);
+    }
+}
