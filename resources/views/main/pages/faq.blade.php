5e36e95dd23069700e8375b0317bb8e5129ccc1a
Salohiddin <berdiyarovsalohiddin@gmail.com>
2021-08-10 14:41:33 +0500

    #main Updated faq.blade.php file

--- resources/views/main/pages/faq.blade.php
+++ resources/views/main/pages/faq.blade.php
@@ -9,40 +9,30 @@
                 <div class="block__wrapper uk-width-1-1">
                     <!-- Container title -->
                     <h1 class="treatment-article__title block__title">Часто задаваемые вопросы</h1>
-                    <div uk-filter="target: .js-filter" class="accordion">
-
-                        <ul class="accordion__btns questions__theme-nav uk-subnav uk-subnav-pill uk-flex uk-flex-center">
+                    <div uk-filter="target: .js-filter">
+                        <ul class="uk-subnav uk-subnav-pill">
                             @foreach($tags as $tagKey => $tagTitle)
-                                <li @if (array_key_first($tags) == $tagKey)
-                                    class="uk-active"
-                                    @endif
-                                    uk-filter-control="[data-theme='{{ $tagKey }}']">
-                                    <a class="btn" href="#">{{ $tagTitle }}</a>
-                                </li>
+                                <li @if ($loop->first) class="uk-active"
+                                    @endif uk-filter-control="[data-tags*='{{ $tagKey }}']"><a
+                                        href="#">{{ $tagTitle }}</a></li>
                             @endforeach
                         </ul>
-
-                        <ul class="js-filter uk-child-width-1-1 uk-child-width-1-2@m uk-text-center accordion__items-container"
-                            uk-grid="masonry: true">
-                            <!-- data-theme-1 -->
-                            @foreach($faqs as $faq)
-                                @foreach($faq->tags as $tag)
-                                    <li data-theme="{{ $tag->id }}" class="accordion__items-element">
-                                        <div class="uk-card uk-card-default uk-card-body accordion__item-card">
-                                            <ul uk-accordion>
-                                                <li>
-                                                    <a class="uk-accordion-title accordion__item-title uk-text-left"
-                                                       href="#">{{ $faq->title }}</a>
-                                                    <div class="uk-accordion-content accordion__item-content">
-                                                        <p>{{ $faq->description }}</p>
-                                                    </div>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </li>
+                        <div class="accord__wrapper">
+                            <ul class="js-filter uk-child-width-1 uk-child-width-1@m uk-text-center" uk-grid
+                                uk-accordion="multiple: true">
+                                @foreach($faqs as $faq)
+                                    @foreach($faq->tags as $tag)
+                                        <li data-tags="{{ $tag->id }}">
+                                            <a class="uk-accordion-title  accordion__item-title"
+                                               href="#">{{ $faq->title }}</a>
+                                            <div class="uk-accordion-content">
+                                                <p>{{ $faq->description }}</p>
+                                            </div>
+                                        </li>
+                                    @endforeach
                                 @endforeach
-                            @endforeach
-                        </ul>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
