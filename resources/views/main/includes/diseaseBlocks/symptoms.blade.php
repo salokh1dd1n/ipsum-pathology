4a363a9ea5e21d82a4e047af5ec5ac13360bc7af
Salohiddin <berdiyarovsalohiddin@gmail.com>
2021-08-10 14:42:00 +0500

    #main Updated symptoms.blade.php file

--- resources/views/main/includes/diseaseBlocks/symptoms.blade.php
+++ resources/views/main/includes/diseaseBlocks/symptoms.blade.php
@@ -1,23 +1,30 @@
 <div>
     <h1 class="treatment-article__title block__title">Симптомы</h1>
-    <ul class="js-filter uk-child-width-1-1 uk-child-width-1-2@m uk-text-center accordion__items-container"
-        uk-grid="masonry: true">
-        @foreach ($disease->symptoms as $symptom)
-            <li class="accordion__items-element">
-                <div class="uk-card uk-card-default uk-card-body accordion__item-card">
-                    <ul uk-accordion>
-                        <li>
-                            <a class="uk-accordion-title accordion__item-title uk-text-left"
-                               href="#">{{ $symptom->title }}</a>
-                            <div class="uk-accordion-content accordion__item-content">
-                                <p>{{ $symptom->description }}</p>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </li>
-        @endforeach
-    </ul>
-
+    <div class="accord__wrapper">
+        <ul uk-accordion="multiple: true">
+            @foreach ($disease->symptoms as $symptom)
+                @if ($loop->odd)
+                    <li @if ($loop->first) class="uk-open" @endif>
+                        <a class="uk-accordion-title  accordion__item-title" href="#">{{ $symptom->title }}</a>
+                        <div class="uk-accordion-content">
+                            <p>{{ $symptom->description }}</p>
+                        </div>
+                    </li>
+                @endif
+            @endforeach
+        </ul>
+        <ul uk-accordion="multiple: true">
+            @foreach ($disease->symptoms as $symptom)
+                @if ($loop->even)
+                    <li>
+                        <a class="uk-accordion-title  accordion__item-title" href="#">{{ $symptom->title }}</a>
+                        <div class="uk-accordion-content">
+                            <p>{{ $symptom->description }}</p>
+                        </div>
+                    </li>
+                @endif
+            @endforeach
+        </ul>
+    </div>
     <p>{{ $disease->symptom_desc }}</p>
 </div>
