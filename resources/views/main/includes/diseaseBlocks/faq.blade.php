cd1ef07d831269f735a526ee5d2c226a421b6575
Salohiddin <berdiyarovsalohiddin@gmail.com>
2021-08-10 14:41:25 +0500

    #main Updated diseaseBlock faq.blade.php file

--- resources/views/main/includes/diseaseBlocks/faq.blade.php
+++ resources/views/main/includes/diseaseBlocks/faq.blade.php
@@ -3,19 +3,28 @@
         <div class="block__wrapper uk-width-1-1">
             <!-- Container title -->
             <h1 class="treatment-article__title block__title">@lang('main.diseases.article.faqTitle')</h1>
-            <div uk-filter="target: .js-filter" class="accordion">
-
-                <ul class="accordion__btns questions__theme-nav uk-subnav uk-subnav-pill uk-flex uk-flex-center">
+            <div uk-filter="target: .js-filter">
+                <ul class="uk-subnav uk-subnav-pill">
                     @foreach($tags as $tagKey => $tagTitle)
-                        <li @if (array_key_first($tags) == $tagKey)
-                            class="uk-active"
-                            @endif
-                            uk-filter-control="[data-theme='{{ $tagKey }}']">
-                            <a class="btn" href="#">{{ $tagTitle }}</a>
-                        </li>
+                        <li @if ($loop->first) class="uk-active"
+                            @endif uk-filter-control="[data-tags*='{{ $tagKey }}']"><a href="#">{{ $tagTitle }}</a></li>
                     @endforeach
                 </ul>
-
+                <div class="accord__wrapper">
+                    <ul class="js-filter uk-child-width-1 uk-child-width-1@m uk-text-center" uk-grid
+                        uk-accordion="multiple: true">
+                        @foreach($disease->faq as $faq)
+                            @foreach($faq->tags as $tag)
+                                <li data-tags="{{ $tag->id }}">
+                                    <a class="uk-accordion-title  accordion__item-title" href="#">{{ $faq->title }}</a>
+                                    <div class="uk-accordion-content">
+                                        <p>{{ $faq->description }}</p>
+                                    </div>
+                                </li>
+                            @endforeach
+                        @endforeach
+                    </ul>
+                </div>
                 <ul class="js-filter uk-child-width-1-1 uk-child-width-1-2@m uk-text-center accordion__items-container"
                     uk-grid="masonry: true">
                     <!-- data-theme-1 -->
